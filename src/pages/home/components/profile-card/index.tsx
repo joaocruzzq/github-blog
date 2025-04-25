9c60d966591e4b5fb017b9c2a9e2f4a1346c9d21
joão pedro cruz <joaocruzzdev@gmail.com>
2025-04-25 13:30:31 -0300

    buscando dados do usuario e renderizando no profile card

--- src/pages/home/components/profile-card/index.tsx
+++ src/pages/home/components/profile-card/index.tsx
@@ -1,19 +1,46 @@
 import { GitUserInfo, ProfileContainer, ProfileContenet, ProfileHeader } from "./styles";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faUpRightFromSquare, faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons"
 
+import { api } from "../../../../lib/axios";
+import { useEffect, useState } from "react";
+
+interface UserProps {
+   bio: string
+   name: string
+   login: string
+   company: string
+   html_url: string
+   followers: number
+   avatar_url: string
+}
+
 export function ProfileCard() {
+   const [userInfo, setUserInfo] = useState<UserProps>()
+
+   async function fetchUser() {
+      const response = await api.get("/users/joaocruzzq")
+      setUserInfo(response.data)
+   }
+
+   useEffect(() => {
+      fetchUser()
+   }, [])
+
    return (
       <ProfileContainer>
-         <img src="https://github.com/joaocruzzq.png" alt="" />
+         <img src={userInfo?.avatar_url} alt="" />
 
          <ProfileContenet>
             <ProfileHeader>
-               <h1>Nome</h1>
+               <h1>
+                  {userInfo?.name}
+               </h1>
 
-               <a href="" target="_blank">
+               <a href={userInfo?.html_url} target="_blank">
                   github
 
                   <FontAwesomeIcon icon={faUpRightFromSquare} />
@@ -21,23 +48,23 @@ export function ProfileCard() {
             </ProfileHeader>
 
             <span>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eligendi hic repellat amet consequatur assumenda nostrum illum adipisci! Dolores velit beatae illo vitae, amet sint repellat earum dolor animi consectetur.
+               {userInfo?.bio}
             </span>
 
             <section>
                <GitUserInfo>
                   <FontAwesomeIcon icon={faGithub} />
-                  github
+                  {userInfo?.login}
                </GitUserInfo>
 
                <GitUserInfo>
                   <FontAwesomeIcon icon={faBuilding} />
-                  trabalho
+                  {userInfo?.company ? userInfo.company : "Freelancer"}
                </GitUserInfo>
 
                <GitUserInfo>
                   <FontAwesomeIcon icon={faUserGroup} />
-                  0 seguidores
+                  {userInfo?.followers} seguidores
                </GitUserInfo>
             </section>
          </ProfileContenet>
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -9,12 +9,12 @@ export function Post() {
          <PostDetailsCard />
 
          <PostMainContent>
-            <p>
-               Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn. <br /> <br />
+            <Markdown>
+               Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
             
-               Dynamic typing <br />
+               Dynamic typing
                JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:
-            </p>
+            </Markdown>
 
             <MarkdownContainer>
                <Markdown>
